0668360fa42d50d386a75fe9c74e25b74370d7e4
David Morrow <quickbasic22@yahoo.com>
2019-10-25 17:42:01 -0400

    First commit

---
+++ MVCAndViewModels/ViewModels/MovieListItemViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAndViewModels.ViewModels
+{
+    public class MovieListItemViewModel
+    {
+        DateTime _ReleaseDate;
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string DayOfRelease
+        {
+            get
+            {
+                return ReleaseDate.DayOfWeek.ToString() + " " + ReleaseDate.ToShortDateString();
+            }
+        }
+        public string Genre { get; set; }
+        public string Rating { get; set; }
+        public string AllTimes { get; set; }
+    }
+}
---
+++ MVCAndViewModels/ViewModels/MovieViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCAndViewModels.ViewModels
+{
+    public class MovieViewModel
+    {
+        public List<String> ListOfGenres { get; set; }
+        public List<String> ListOfRatings{ get; set; }
+        public List<String> Theatre { get; set; }
+        public string Time { get; set; }
+
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Date is required")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Genre must be specified")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Price required")]
+        [Range(1, 100, ErrorMessage = "Price must be between $1 and $100")] 
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal Price { get; set; }
+
+        [StringLength(5)]
+        public string Rating { get; set; }
+    }
+}
